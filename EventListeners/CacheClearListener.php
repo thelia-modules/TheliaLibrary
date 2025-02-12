d1c36e81e4db83a519eea9518388542559bb7589
ArthurLashermes <76913541+ArthurLashermes@users.noreply.github.com>
2025-02-12 10:30:51 +0100

    fix : add image-library in clear cache process (#18)
    Co-authored-by: ArthurLashermes <alashermes@openstudio.fr>

--- EventListeners/CacheClearListener.php
+++ EventListeners/CacheClearListener.php
@@ -22,16 +22,24 @@ class CacheClearListener implements EventSubscriberInterface
         ];
     }
 
-    public function deleteAllTheliaLibraryCachedImages(CacheEvent $event) : void
+    public function deleteAllTheliaLibraryCachedImages(CacheEvent $event): void
     {
-        $cachedPath = THELIA_WEB_DIR.ConfigQuery::read(
+        $cachedPath = THELIA_WEB_DIR . ConfigQuery::read(
                 search: 'image_cache_dir_from_web_root',
-                default: 'cache'.DS.'images'
-        );
-        $cachedDirectory = THELIA_WEB_DIR.'legacy-image-library';
-        if ($event->getDir() === $cachedPath && is_dir($cachedDirectory)) {
-            $imageLibraryEvent = new CacheEvent($cachedDirectory);
-            $this->dispatcher->dispatch(event: $imageLibraryEvent, eventName: TheliaEvents::CACHE_CLEAR);
+                default: 'cache' . DS . 'images'
+            );
+        $cachedDirectoryLegacy = THELIA_WEB_DIR . 'legacy-image-library';
+        $cachedDirectoryLibrary = THELIA_WEB_DIR . 'image-library';
+        if ($event->getDir() !== $cachedPath) {
+            return;
+        }
+        if (is_dir($cachedDirectoryLegacy)) {
+            $imageLibraryEventLegacy = new CacheEvent($cachedDirectoryLegacy);
+            $this->dispatcher->dispatch(event: $imageLibraryEventLegacy, eventName: TheliaEvents::CACHE_CLEAR);
+        }
+        if (is_dir($cachedDirectoryLibrary)) {
+            $imageLibraryEventLibrary = new CacheEvent($cachedDirectoryLibrary);
+            $this->dispatcher->dispatch(event: $imageLibraryEventLibrary, eventName: TheliaEvents::CACHE_CLEAR);
         }
     }
 }
