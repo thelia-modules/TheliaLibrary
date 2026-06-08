c20f1e3d812f23cbdae4da7b9cce05b91963e92e
Alexandre Nozière <anoziere@openstudio.fr>
2026-06-08 21:21:28 +0200

    fix: use __DIR__ in configureServices exclude to support local/modules layout

--- TheliaLibrary.php
+++ TheliaLibrary.php
@@ -98,7 +98,7 @@ class TheliaLibrary extends BaseModule
     public static function configureServices(ServicesConfigurator $servicesConfigurator): void
     {
         $servicesConfigurator->load(self::getModuleCode().'\\', __DIR__)
-            ->exclude([THELIA_MODULE_DIR.ucfirst(self::getModuleCode()).'/I18n/*'])
+            ->exclude([__DIR__.'/I18n/*'])
             ->autowire(true)
             ->autoconfigure(true);
     }
